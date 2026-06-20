0692c5136c1fda0b34c9d11cd15a47f02bb34766
Tejassai <118813828+Tejassai@users.noreply.github.com>
2026-06-20 15:30:44 +0530

    Update Expense.h

--- src/Expense.h
+++ src/Expense.h
@@ -1,1 +1,13 @@
+#ifndef EXPENSE_H
+#define EXPENSE_H
 
+#include <string>
+
+struct Expense {
+    std::string date; // YYYY-MM-DD
+    std::string category;
+    std::string description;
+    double amount;
+};
+
+#endif
